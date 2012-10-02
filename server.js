b7993a1a701663d39b78916dc2bc2d3da2885106
Andrew Wong <th5418@gmail.com>
2012-10-01 19:37:26 -0700

    fixed permissions

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var socket = require('socket.io')
+var http = require('http')
+, socket = require('socket.io')
 , fs = require('fs')
 , ams = require('ams')
 , connect = require('connect')
+, connectnowww = require('connect-no-www')
 , server
 , clientDir = __dirname + '/client'
 , depsDir = __dirname + '/deps'
@@ -23,10 +25,35 @@ function configureFiles() {
 	.end()    
 };
 
+function niceifyURL(req, res, next){
+  if (/^\/game\/public/.exec(req.url)) {
+    res.writeHead(302, {
+      'Location': '/game/#!/' + getLatestPublicGame().hash
+    });
+    return res.end();
+  }
+  if (/^\/game$/.exec(req.url)) {
+    res.writeHead(301, { 'Location': '/game/' });
+    return res.end();
+  }
+  if (/^\/game\//.exec(req.url)) {
+    req.url = '/game.html';
+  } else if (/^\/about/.exec(req.url)) {
+    req.url = '/about.html';
+  } else if (/^\/help/.exec(req.url)) {
+    req.url = '/help.html';
+  } else if (/^\/?$/.exec(req.url)) {
+    req.url = '/index.html';
+  }
+  return next();
+}
+
 configureFiles();
 
 server = connect.createServer(
     connect.logger(':status :remote-addr :url in :response-timems')
-).listen(prod ? 80 : 4567);
+    , connectnowww()
+    , niceifyURL
+).listen(prod ? 80 : 3000);
 
 socket.listen(server);
